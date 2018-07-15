5bdf9c5aa2dbe1acc6d95be0ddf6c10ab76d5c64
정근희 <>
2018-07-15 17:47:04 +0900

    typescript 초기 셋팅

---
+++ test.js
@@ -0,0 +1,11 @@
+var b = "geuni";
+b = 1;
+var Test = /** @class */ (function () {
+    function Test() {
+        // console.log('test');
+    }
+    return Test;
+}());
+// new Test();
+var n = 0;
+n.toString();
